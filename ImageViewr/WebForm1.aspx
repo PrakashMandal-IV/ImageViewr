5d3f21bfba042bd03aa38c1d5a686076548eb560
Prakash Mandal <prakashmandal.iv@gmail.com>
2022-06-08 21:41:34 +0530

    Add project files.

---
+++ ImageViewr/WebForm1.aspx
@@ -0,0 +1,45 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="ImageViewr.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Image Manager</title>
+    <link rel="stylesheet" href="Css/StyleSheet1.css" />
+</head>
+<body>
+    <form runat="server">
+        <div class="container">
+        <div class="first_part">
+            <div class="Datatable">
+            <asp:GridView ID="ImageData"  CssClass="Grid table table-condensed table-responsive table-hover" OnSelectedIndexChanged="GridView1_SelectedIndexChanged" runat="server" Height="16px" Width="486px" Style="margin-right: 0px; margin-top: 29px;" GridLines="none" AllowSorting="True">
+                <HeaderStyle BackColor="black" ForeColor="White" Font-Names="Roboto" Height="30pt" />
+                <RowStyle />
+                <Columns>
+                    <asp:TemplateField>
+                        <ItemTemplate>
+                            <asp:Button ID="ViewBtn" CssClass="Button" runat="server" Style="border: none; border-radius: 5px; background-image: linear-gradient(to bottom, rgb(133, 133, 190), rgb(96, 96, 255)); color: white; width: 100pt" BorderStyle="None" Height="20px" Text="View" Value='<%#Eval("Id") %>' CommandName="Select" />
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                </Columns>
+            </asp:GridView>
+                </div>
+                </div>
+                        <h1>Image Section</h1>
+            <div class="ImageSelection">
+                <p>Choose Image</p>
+                <asp:FileUpload ID="ImageSelector" Class="ImgSelector" runat="server" />
+            </div>
+            <asp:Label ID="Info" runat="server"></asp:Label>
+            <asp:Label ID="msg" runat="server"></asp:Label>
+            <asp:Button ID="Button1" runat="server" Text="" />
+            <asp:Label ID="successMsg" runat="server" Text=""></asp:Label>
+        
+            <div style="margin: 20pt">
+                <asp:Image ID="ImageOrg" runat="server" />
+                <asp:Image ID="Thumbnail" runat="server" />
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ImageViewr/WebForm1.aspx.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ImageViewr;
+
+namespace ImageViewr
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        SqlConnection _connection = new SqlConnection(ConfigurationManager.ConnectionStrings["SqlDatabase"].ConnectionString);
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                GetAllImages();
+            }
+        }
+
+        protected void Upload_Click(object sender, EventArgs e)
+        {
+            UploadImage();
+        }
+
+
+        protected void UploadImage()
+        {
+            try
+            {
+                string originalFilenName = Path.GetFileName(ImageSelector.FileName);
+                //Check if the file is selected or not 
+                if (ImageSelector.HasFile)
+                {
+                    string fileExtension = Path.GetExtension(ImageSelector.FileName).ToLower();
+                    string date = DateTime.Now.ToShortDateString();
+                    //Check the file type
+                    if (fileExtension == ".jpg" || fileExtension == ".png" || fileExtension == ".jpeg")
+                    {
+                        string filepath = Server.MapPath("\\CDN\\Image\\" + date + "\\Original\\");
+                        string filepathfordb = "\\CDN\\Image\\" + date + "\\Original\\";
+                        //check if directory exist or not
+                        if (!Directory.Exists(filepath))
+                        {
+                            Directory.CreateDirectory(filepath);
+                        }
+                        string newFileName = SaveImageToDB(filepathfordb, originalFilenName, fileExtension);
+                        ImageSelector.SaveAs(filepath + newFileName);
+                        ThumbnailGenerateor(filepath, newFileName);
+                        successMsg.Text = "Successfully Uploaded !";
+                        GetAllImages();
+                    }
+                    else msg.Text = "File must be png/jpg/jpeg";
+                }
+                else msg.Text = "Please Select a file";
+            }
+            catch (Exception ex)
+            {
+                msg.Text = ex.Message;
+            }
+        }
+
+        public void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            getFilePath(Convert.ToInt32(ImageData.SelectedRow.Cells[1].Text));
+        }
+
+
+
+        //Method to store file in Database
+        protected string SaveImageToDB(string path, string filename, string filetype)
+        {
+            DateTime date = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+
+            //ADD FILE TO DATABASE
+            SqlCommand query = new SqlCommand("exec stp_AddImage '" + filename + "','" + path + "','" + date + "'", _connection);
+            _connection.Open();
+            int id = Convert.ToInt32(query.ExecuteScalar().ToString());
+            _connection.Close();
+
+            string NameDate = DateTime.Now.ToString("yyyyMMddhhmmss");
+            string Newfilename = id + "-" + NameDate + filetype;
+            string newPath = path + Newfilename;
+
+            // Update File Path to DATABASE
+            UpdatePath(id, newPath);
+            return Newfilename;
+        }
+        protected void UpdatePath(int id, string newPath)
+        {
+            _connection.Open();
+            SqlCommand UpdateQuery = new SqlCommand("exec stp_UpdateImagePath '" + id + "','" + newPath + "'", _connection);
+            UpdateQuery.ExecuteNonQuery();
+            _connection.Close();
+        }
+        protected void GetAllImages()
+        {
+            SqlCommand query = new SqlCommand(" exec stp_GetAllIamges", _connection);
+            SqlDataAdapter sd = new SqlDataAdapter(query);
+            DataTable dt = new DataTable();
+            sd.Fill(dt);
+            dt.Columns.Remove("FilePath");
+            ImageData.DataSource = dt;
+            ImageData.DataBind();
+        }
+
+
+        protected void getFilePath(int id)
+        {
+            SqlCommand query = new SqlCommand(" exec stp_GetFilePathOfSelectedIamge '" + id + "'", _connection);
+            _connection.Open();
+            string path = query.ExecuteScalar().ToString();
+            _connection.Close();
+            ImageOrg.ImageUrl = path;
+            string thumbnailPart = path.Replace("Original", "Thumbnail");
+            Thumbnail.ImageUrl = thumbnailPart;
+
+        }
+
+        protected void ThumbnailGenerateor(string filepath, string name)
+        {
+            Imager.PerformImageResizeAndPutOnCanvas(filepath, name, name);
+        }
+    }
+}
